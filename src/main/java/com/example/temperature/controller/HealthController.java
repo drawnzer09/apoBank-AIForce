749ebc008793beb58469455f79099ac5a54b87c5
drawnzer09 <shubhamdaheriya626@gmail.com>
2026-07-13 11:11:51 +0530

    Update HealthController.java

--- src/main/java/com/example/temperature/controller/HealthController.java
+++ src/main/java/com/example/temperature/controller/HealthController.java
@@ -5,7 +5,7 @@ import com.example.temperature.service.HealthService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+{this won't compile.}
 @RestController
 @RequestMapping(path = "/v1/health", produces = "application/json")
 public class HealthController {
